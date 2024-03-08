7ad3f7c13c000e70ba281e37883a0179c0e94e69
Victor <victorrubenlr@gmail.com>
2024-03-08 12:32:09 -0300

    Configurado para escrever nas informações

--- codigo.py
+++ codigo.py
@@ -4,6 +4,7 @@
     # https://dlp.hashtagtreinamentos.com/python/intensivao/login
 import pyautogui
 import time
+import pandas
 
 pyautogui.PAUSE = 0.5
 
@@ -29,9 +30,47 @@ pyautogui.write("senha123")
 pyautogui.press("enter")
 
 # Passo 3: Importar a base de dados
+    #delay
+time.sleep(3)
 
-
+    #importar tabela
+tabela = pandas.read_csv("produtos.csv")
 
 # Passo 4: Cadastrar um produto
+    #clicar no 1º campo
+pyautogui.click(x=688, y=258)
+
+    #cadastro
+
+        #Codigo
+pyautogui.write()
+pyautogui.press("tab")
+
+        #Marca
+pyautogui.write()
+pyautogui.press("tab")
+
+        #Tipo
+pyautogui.write()
+pyautogui.press("tab")
+
+        #Categoria
+pyautogui.write()
+pyautogui.press("tab")
+
+        #Preço
+pyautogui.write()
+pyautogui.press("tab")
+
+        #Custo
+pyautogui.write()
+pyautogui.press("tab")
+
+        #OBS
+pyautogui.write()
+pyautogui.press("tab")
+
+        #Enviar
+pyautogui.press("enter")
 
 # Passo 5: Repetir o processo até acabar
