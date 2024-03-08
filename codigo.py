738a61ddc8825f3071a5145e71a0bbe6763ef0fb
Victor <victorrubenlr@gmail.com>
2024-03-08 11:51:01 -0300

    Adicionado entrada no site

--- codigo.py
+++ codigo.py
@@ -3,13 +3,20 @@
 # Passo 1: Entrar no sistema da empresa
     # https://dlp.hashtagtreinamentos.com/python/intensivao/login
 import pyautogui
+pyautogui.PAUSE = 0.5
 
 #abrir navegador
 pyautogui.press("win")
 pyautogui.write("firefox")
 pyautogui.press("enter")
 
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+pyautogui.write(link)
+pyautogui.press("enter")
+
 # Passo 2: Fazer Login
+
+
 
 # Passo 3: Importar a base de dados
 
