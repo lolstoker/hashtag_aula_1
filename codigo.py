b2c3a4e9a78e0ef893c68b75a188cf780f0a973c
Victor <victorrubenlr@gmail.com>
2024-03-08 11:38:15 -0300

    Adicionado codigo para entrar no navegador.

---
+++ codigo.py
@@ -0,0 +1,18 @@
+# Passo a passo do projeto
+
+# Passo 1: Entrar no sistema da empresa
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+import pyautogui
+
+#abrir navegador
+pyautogui.press("win")
+pyautogui.write("firefox")
+pyautogui.press("enter")
+
+# Passo 2: Fazer Login
+
+# Passo 3: Importar a base de dados
+
+# Passo 4: Cadastrar um produto
+
+# Passo 5: Repetir o processo até acabar
