2e1597931eec91589c72b2355197c0c18505dc8d
Victor <victorrubenlr@gmail.com>
2024-03-08 12:09:41 -0300

    Adicionado login e senha automatico

--- codigo.py
+++ codigo.py
@@ -3,9 +3,11 @@
 # Passo 1: Entrar no sistema da empresa
     # https://dlp.hashtagtreinamentos.com/python/intensivao/login
 import pyautogui
+import time
+
 pyautogui.PAUSE = 0.5
 
-#abrir navegador
+    #abrir navegador
 pyautogui.press("win")
 pyautogui.write("firefox")
 pyautogui.press("enter")
@@ -14,11 +16,21 @@ link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(link)
 pyautogui.press("enter")
 
+    #Delay
+time.sleep(3)
+
 # Passo 2: Fazer Login
-
-
+    #login
+pyautogui.click(x=681, y=376)
+pyautogui.write("usuario@gmail.com")
+    #senha
+pyautogui.press("tab")
+pyautogui.write("senha123")
+pyautogui.press("enter")
 
 # Passo 3: Importar a base de dados
+
+
 
 # Passo 4: Cadastrar um produto
 
